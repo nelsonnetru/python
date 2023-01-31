6ddc41968bf5b1f2d4fe10971863714e38f697c2
Aleksandr Ivanov <3911394@mail.ru>
2023-01-31 14:01:10 +0300

    Homework_02 added

---
+++ homework_02/002.py
@@ -0,0 +1,24 @@
+﻿'''
+Задача 2: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. Выведите минимальное количество монет, которые нужно перевернуть.
+'''
+import random
+
+n = int(input('Количество монет: '))
+if n < 1: exit()
+
+sideName = ['герб', 'решка'] # 0 - герб, 1 - решка
+
+monets = [random.randint(0, 1) for i in range(n)]
+print(monets)
+
+print('\nВыпавшие монеты:')
+for side in monets:
+	print(f'{side} - ' + sideName[side])
+
+minimalSide = 0 if monets.count(0) < monets.count(1) else 1
+reverseMonets = monets.count(minimalSide)
+
+print(f'\nМинимальное количество монет для переворота: {reverseMonets}')
+
+if reverseMonets > 0:
+	print(f'Нужно перевернуть монеты, которые лежат стороной: {sideName[minimalSide]}')
