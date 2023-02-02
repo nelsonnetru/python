0041e0922e7153ffd3144fe41764468acb8ac1ba
Aleksandr Ivanov <3911394@mail.ru>
2023-02-02 11:11:20 +0300

    trainig

---
+++ training/homework_02_monets/files_mod.py
@@ -0,0 +1,32 @@
+﻿def WriteToFile(data):
+	file = open('data.txt', 'a')
+	if type(data) is list:
+		file.writelines(data)
+	else:
+		file.write(data)
+	file.close()
+
+def ReadFromFile():
+	with open('data.txt', 'r') as file:
+		for line in file:
+			print (line.strip())
+
+def ReadFromFileMonets():
+	with open('data.txt', 'r') as file:
+		print ('Данные из файла:')
+		i = 1
+		for line in file:
+			print('\n\n' + '-'*10 + f' набор {i} ' + '-'*10)
+			monetsStringList = line.strip()[1:-1].split(', ')
+			monetsIntList = [int(i) for i in monetsStringList]
+			monets.ReadMonets(monetsIntList)
+			i += 1
+
+
+if __name__ == '__main__':
+	#WriteToFile('строка 2\n')
+	#ReadFromFile()
+	import monets
+	ReadFromFileMonets()
+
+	
---
+++ training/homework_02_monets/monets.py
@@ -0,0 +1,38 @@
+﻿'''
+Задача 2: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом. 
+Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты 
+вверх одной и той же стороной. Выведите минимальное количество монет, которые нужно перевернуть.
+'''
+import random, files_mod, os, sys
+os.system('cls')
+
+def ReadMonets (monets = list(), sideName = ['герб', 'решка']): # 0 - герб, 1 - решка
+	print('\nВыпавшие монеты:')
+	for side in monets:
+		print(f'{side} - ' + sideName[side])
+
+	minimalSide = 0 if monets.count(0) < monets.count(1) else 1
+	reverseMonets = monets.count(minimalSide)
+
+	print(f'\nМинимальное количество монет для переворота: {reverseMonets}')
+
+	if reverseMonets > 0:
+		print(f'Нужно перевернуть монеты, которые лежат стороной: {sideName[minimalSide]}')
+
+def GenerateMonets (n = 5, toFile = False):
+	print (f'Количество монет: {n}')
+	if n < 1: return
+
+	monets = [random.randint(0, 1) for i in range(n)]
+	print(monets)
+	if toFile:
+		print('Запись в файл...')
+		files_mod.WriteToFile(str(monets) + '\n')
+		print('Готово.')
+	else:
+		ReadMonets(monets)
+
+if __name__ == '__main__':
+	tofile = False
+	if 'tofile' in sys.argv: tofile = True
+	GenerateMonets(7, tofile)
